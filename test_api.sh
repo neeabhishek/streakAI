f629b82af680b38321ec86c83da03e5abd388005
Abhishek Neelakandan <64977046+neeabhishek@users.noreply.github.com>
2025-03-20 16:25:44 +0530

    Commit - APITesting

---
+++ test_api.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+#Starting the Container
+echo "** Starting the Container **" 
+docker run -it -d -p 8077:5000 --name=StreakAI neeabhishek/streak_ai:=latest
+
+#Validate the Container
+echo "** Validating the Container **"
+CONTAINER_RESPONSE=$(docker ps | grep streak_ai)
+echo "Container Response - ${CONTAINER_RESPONSE}"
+
+echo "** Waiting for 5 sec for API Testing **"
+sleep 5
+
+#API Respone capture 
+API_RESPONSE=$(curl "http://localhost:8077/add?num1=10&num2=20")
+
+if [ "$API_RESPONSE" == "30" ]; then
+    exit 0
+else
+    exit 1
+fi
